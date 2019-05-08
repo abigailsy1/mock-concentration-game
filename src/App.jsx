5db1716893835e09ec6b5f37a7d4e4fd8b65c36b
Abby <abigailsy1@yahoo.com>
2019-05-08 16:49:15 -0700

    added photos, updated card component, and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Readme from './readme';
+import Card from './components/card'
 
 export default function App() {
+  const [flipped, setFlipped] = useState([])
+
+  const handleClick = (id) => setFlipped([...flipped, id])
+
   return (
     <div>
       <h1>Mock Concentration Game</h1>
       <p>Are you ready to test your memory?</p>
+
+      <Card 
+        id={1}
+        width={100}
+        height={100}
+        back={`/img/back.png`}
+        front={`/img/Chopper.jpg`}
+        flipped={flipped.includes(1)}
+        handleClick={() => handleClick(1)}/>
     </div>
   )
 }
---
+++ src/components/card/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './styles.css'
+
+export default function Card({
+    handleClick,
+    id,
+    flipped,
+    back,
+    front,
+    height,
+    width
+}) {
+    return <div
+        className={`flip-container ${flipped ? 'flipped' : ''}`}
+        style={{
+            height,
+            width
+        }}
+        onClick={() => handleClick(id)}>
+
+        <div className="flipper">
+            <img
+                style={{
+                    height,
+                    width
+                }}
+                className={flipped ? 'front' : 'back'}
+                src={flipped ? front : back} />
+        </div>
+    </div>
+}
+
+Card.propTypes = {
+    handleClick: PropTypes.bool.isRequired,
+    id:PropTypes.number.isRequired,
+    flipped: PropTypes.bool.isRequired,
+    back: PropTypes.string.isRequired,
+    front: PropTypes.string.isRequired,
+    height: PropTypes.number.isRequired,
+    width:PropTypes.number.isRequired
+}
