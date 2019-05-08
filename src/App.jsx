b2ccf2a74e8eb7f0825c215c671ca93244e7a1c6
Abby <abigailsy1@yahoo.com>
2019-05-07 18:42:54 -0700

    initial start up of mock app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Readme from './readme';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Mock Concentration Game</h1>
+      <p>Are you ready to test your memory?</p>
+    </div>
+  )
+}
